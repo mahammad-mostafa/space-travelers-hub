bdfaf1cb91b58dbab6763b228ac03d77cdb2166d
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-05 22:58:51 +0300

    Implemented redux store configuration with logger
    Co-Authored-By: Gumaro Monroy <maromvz@gmail.com>

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+const store = configureStore({
+  reducer: {}, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
