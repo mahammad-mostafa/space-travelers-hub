4499250f21530f38f4fc9277aaaab8ed5369fdd3
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-06 21:23:45 +0300

    Implemented component  rendering with styling

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,3 +1,27 @@
-const Profile = () => <h1>Profile</h1>;
+import { useSelector } from 'react-redux';
+import Rocket from './rocket';
+import Indicator from './indicator';
+import Styles from '../styles/profile.module.css';
+
+const Profile = () => {
+  let rockets = useSelector((state) => state.rockets.list);
+  rockets = rockets.filter((item) => item.reserved === true);
+  return (
+    <section className={Styles.section}>
+      <div className={Styles.block}>
+        <h2 className={Styles.title}>My Rockets</h2>
+        {rockets.length === 0 ? (
+          <div className={Styles.empty}>
+            <Indicator error="No Rockets Reserved" />
+          </div>
+        ) : (
+          <ul className={Styles.list}>
+            {rockets.map((item) => <Rocket key={item.id} rocket={item} profile />)}
+          </ul>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default Profile;
