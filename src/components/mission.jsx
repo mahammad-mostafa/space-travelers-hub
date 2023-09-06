48d6d39e028018f70ed894a0e0723db4ccd08ff3
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-09-06 12:45:02 -0600

    Add missions list and item components

--- src/components/mission.jsx
+++ src/components/mission.jsx
@@ -1,3 +1,25 @@
-const Missions = () => <h1>Missions</h1>;
+import { useDispatch } from "react-redux";
+import { toggle } from "../slices/missionSlice";
 
-export default Missions;
+const Mission = ({ mission }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="row">
+      <div className="col mission-name">
+        <h3>{mission.name}</h3>
+      </div>
+      <div className="col-large mission-desc">
+        <p>{mission.description}</p>
+      </div>
+      <div className="col mission-member">
+        <p>{mission.member ? "Active Member" : "NOT A MEMBER"}</p>
+      </div>
+      <div className="col mission-join">
+        <button type="button" onClick={() => dispatch(toggle(mission.id))}>{mission.member ? "Leave" : "Join"}</button>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
